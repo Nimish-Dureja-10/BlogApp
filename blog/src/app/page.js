85f26f267e0b74678e2e0ecef9f9e876701a4bba
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-01-19 12:20:04 +0530

    Homepage and footer updated

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,6 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Blog Site</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Facere eius animi vel eum commodi autem reiciendis, blanditiis
+          eveniet repudiandae repellendus!
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src={"/brands.png"} alt="Brand_images" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src={"/hero.gif"} alt="Hero_image" fill className={styles.heroImg} />
+      </div>
+    </div>
   )
 }
 
