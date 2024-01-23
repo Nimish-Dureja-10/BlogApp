711f4d90e45d3753c2884d356283bbd6399d3351
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-01-23 23:02:16 +0530

    contact page updated

--- blog/src/app/contact/page.jsx
+++ blog/src/app/contact/page.jsx
@@ -1,6 +1,22 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={"/contact.png"} alt="contact_img" className={styles.img} fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type={"text"} placeholder="Full Name" />
+          <input type={"email"} placeholder="Your Email Address" />
+          <input type={"text"} placeholder="Phone Number" />
+          <textarea cols={'30'} rows="10" placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
